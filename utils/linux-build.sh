6ad2c47b8e83a5772d9f1a8b82f0f59d035faebb
bouajilaprog <bouajilamedyessine@gmail.com>
2025-12-16 02:02:00 +0100

    feat(instalation): update the new linux-build script

--- utils/linux-build.sh
+++ utils/linux-build.sh
@@ -1,3 +1,6 @@
-pyinstaller --onefile main.py
-strip dist/main
-mv dist/main dist/code-down
+pyinstaller --onefile -n code-down src/codedown/__main__.py
+strip dist/code-down
+if [ ! -d build/linux-build ]; then
+  mkdir -p build/linux-build
+fi
+mv dist/code-down build/linux-build
